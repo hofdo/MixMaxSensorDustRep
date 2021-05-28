870f979107a09a8564e0509878a89de4e71fef0c
hofmd2 <hofmann.dominique@bluewin.ch>
2021-05-28 13:31:41 +0200

    first commit

---
+++ python/dragino/FrequncyPlan.py
@@ -0,0 +1,30 @@
+###############################################################################
+#Details of the frequncies available for LoRaWAN usage
+#Copyright (C) 2018 Philip Basford
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as
+#published by the Free Software Foundation, either version 3 of the
+#License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
+
+#https://www.thethingsnetwork.org/wiki/LoRaWAN/Frequencies/Frequency-Plans
+CH1 = 868.1
+CH2 = 868.1
+CH3 = 868.1
+CH4 = 868.1
+CH5 = 868.1
+CH6 = 868.1
+CH7 = 868.1
+CH8 = 868.1
+
+LORA_FREQS = (CH1, CH2, CH3, CH4, CH5, CH6, CH7, CH8)
+JOIN_FREQS = (CH1, CH2, CH3)
